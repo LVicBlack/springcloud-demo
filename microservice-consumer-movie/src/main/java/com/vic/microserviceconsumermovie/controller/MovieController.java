1577935627df461be445a30661759d07dbbc22ad
vic-company <1173646430@qq.com>
2018-12-21 18:02:12 +0800

    v1.3解决Spring Could 2.0 eureka 配置spring.security后其他服务无法连接注册中心

--- microservice-consumer-movie/src/main/java/com/vic/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/vic/microserviceconsumermovie/controller/MovieController.java
@@ -1,5 +1,7 @@
 package com.vic.microserviceconsumermovie.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
 import com.vic.microserviceconsumermovie.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +9,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
         return restTemplate.getForObject("http://localhost:8010/" + id, User.class);
+    }
+
+    @GetMapping("/user-instance")
+    public List<InstanceInfo> showInfo() {
+        return discoveryClient.getInstancesById("microservice-provider-user");
     }
 }
