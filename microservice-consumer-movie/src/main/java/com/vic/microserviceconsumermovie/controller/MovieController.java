063dbc5b686e92437568f4a925af9186ab9cc585
vic-company <1173646430@qq.com>
2018-12-24 17:32:36 +0800

    v1.3 Eureka

--- microservice-consumer-movie/src/main/java/com/vic/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/vic/microserviceconsumermovie/controller/MovieController.java
@@ -1,9 +1,9 @@
 package com.vic.microserviceconsumermovie.controller;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
 import com.vic.microserviceconsumermovie.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +19,13 @@ public class MovieController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    @GetMapping("/user/{id}")
+    @GetMapping(value = "/user/{id}", produces = "application/json")
     public User findById(@PathVariable Long id) {
-        return restTemplate.getForObject("http://localhost:8010/" + id, User.class);
+        return restTemplate.getForObject("http://localhost:8011/" + id, User.class);
     }
 
-    @GetMapping("/user-instance")
-    public List<InstanceInfo> showInfo() {
-        return discoveryClient.getInstancesById("microservice-provider-user");
+    @GetMapping(value = "/user-instance", produces = "application/json")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("microservice-provider-user");
     }
 }
