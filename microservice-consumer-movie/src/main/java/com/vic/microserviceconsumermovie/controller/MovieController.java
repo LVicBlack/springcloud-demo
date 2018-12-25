43de2f4742a6657ac5d22b9afc1a659369b8ea33
vic-company <1173646430@qq.com>
2018-12-25 17:56:11 +0800

    v2.0 添加Ribbon

--- microservice-consumer-movie/src/main/java/com/vic/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/vic/microserviceconsumermovie/controller/MovieController.java
@@ -1,9 +1,12 @@
 package com.vic.microserviceconsumermovie.controller;
 
 import com.vic.microserviceconsumermovie.domain.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +20,32 @@ public class MovieController {
     private RestTemplate restTemplate;
 
     @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
     private DiscoveryClient discoveryClient;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 
     @GetMapping(value = "/user/{id}", produces = "application/json")
     public User findById(@PathVariable Long id) {
         return restTemplate.getForObject("http://localhost:8011/" + id, User.class);
     }
 
+    /**
+     * 查询microservice-provider-user服务的信息并返回
+     * @return microservice-provider-user服务的信息
+     */
     @GetMapping(value = "/user-instance", produces = "application/json")
     public List<ServiceInstance> showInfo() {
         return discoveryClient.getInstances("microservice-provider-user");
+    }
+
+    // Ribbon
+    @GetMapping(value = "/log-instance", produces = "application/json")
+    public ServiceInstance logUserInstance() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("microservice-provider-user");
+        LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(),serviceInstance.getHost(), serviceInstance.getPort());
+        return serviceInstance;
     }
 }
