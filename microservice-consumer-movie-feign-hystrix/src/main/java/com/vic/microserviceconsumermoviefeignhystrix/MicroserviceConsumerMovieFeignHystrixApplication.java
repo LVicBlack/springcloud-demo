90dd10c0b2354caebb53babfede5e9dc6e79d50c
vic-company <1173646430@qq.com>
2019-02-13 17:28:12 +0800

    v5.0 Feign使用Hystrix--为Feign添加回退

---
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/vic/microserviceconsumermoviefeignhystrix/MicroserviceConsumerMovieFeignHystrixApplication.java
@@ -0,0 +1,21 @@
+package com.vic.microserviceconsumermoviefeignhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class MicroserviceConsumerMovieFeignHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConsumerMovieFeignHystrixApplication.class, args);
+	}
+
+}
+
---
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/vic/microserviceconsumermoviefeignhystrix/controller/MovieController.java
@@ -0,0 +1,54 @@
+package com.vic.microserviceconsumermoviefeignhystrix.controller;
+
+import com.vic.microserviceconsumermoviefeignhystrix.domain.entity.User;
+import com.vic.microserviceconsumermoviefeignhystrix.feign.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
+    // 使用Feign进行请求
+    @GetMapping(value = "/feign/user/{id}", produces = "application/json")
+    public User feignFindById(@PathVariable Long id) {
+        return userFeignClient.findById(id);
+    }
+
+    /**
+     * 查询microservice-provider-user服务的信息并返回
+     * @return microservice-provider-user服务的信息
+     */
+    @GetMapping(value = "/user-instance", produces = "application/json")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("microservice-provider-user");
+    }
+
+    // Ribbon
+    @GetMapping(value = "/log-instance", produces = "application/json")
+    public ServiceInstance logUserInstance() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("microservice-provider-user");
+        LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(),serviceInstance.getHost(), serviceInstance.getPort());
+        return serviceInstance;
+    }
+}
