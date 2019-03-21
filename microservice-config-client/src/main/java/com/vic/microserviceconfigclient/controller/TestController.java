2299ed6f4182cb629ec4a6293d1af56cd00871b3
vic <1173646430@qq.com>
2019-03-21 23:03:11 +0800

    .

--- microservice-config-client/src/main/java/com/vic/microserviceconfigclient/controller/TestController.java
+++ microservice-config-client/src/main/java/com/vic/microserviceconfigclient/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.vic.microserviceconfigclient.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Vic on 2019/3/12
  */
 @RestController
+@RefreshScope
 public class TestController {
     @Value("${profile}")
     private String profile;
-
-
 
     @GetMapping("/profile")
     public String hello() {
